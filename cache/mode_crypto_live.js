19b80d23de7379d4306c186e0f7acbd0f02a8d60
collecte-pc <collecte@users.noreply.github.com>
2026-08-28 06:01:36 +0200

    donnees: annees corrigees, cours de l univers, bilans complets

--- cache/mode_crypto_live.js
+++ cache/mode_crypto_live.js
@@ -1,1 +1,1 @@
-window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":22.02,"price":80797.0,"ma":66215.0,"perf_30d":26.41,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-08-28T01:35:45.838958+00:00"};
+window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":20.71,"price":79918.0,"ma":66206.0,"perf_30d":25.04,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-08-28T03:36:56.286307+00:00"};
