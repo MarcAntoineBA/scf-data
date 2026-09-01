112884e416bff608fdc3d3ef0380d707e69b09f9
collecte <collecte@users.noreply.github.com>
2026-09-01 22:58:20 +0000

    données 6h : 37 fichier(s)

--- cache/mode_crypto_live.js
+++ cache/mode_crypto_live.js
@@ -1,1 +1,1 @@
-window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":17.02,"price":77509.0,"ma":66237.0,"perf_30d":22.05,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-09-01T17:29:47.393186+00:00"};
+window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":16.59,"price":77225.0,"ma":66234.0,"perf_30d":21.61,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-09-01T21:59:30.636424+00:00"};
