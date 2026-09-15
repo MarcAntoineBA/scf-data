862d7e2e9c3f65521a4862a45d214d2435ab2e68
collecte <collecte@users.noreply.github.com>
2026-09-15 23:37:45 +0000

    données 6h : 38 fichier(s)

--- cache/mode_crypto_live.js
+++ cache/mode_crypto_live.js
@@ -1,1 +1,1 @@
-window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":13.72,"price":76734.0,"ma":67476.0,"perf_30d":22.1,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-09-15T17:51:10.905181+00:00"};
+window.__MODE_CRYPTO_LIVE__={"mode":"alpha","dist_pct":11.41,"price":75154.0,"ma":67460.0,"perf_30d":19.59,"ref_asset":"BTC","ma_label":"MA100","updated":"2026-09-15T22:27:43.683249+00:00"};
--- cache/mode_tradfi_live.js
+++ cache/mode_tradfi_live.js
@@ -1,1 +1,1 @@
-window.__MODE_TRADFI_LIVE__={"mode":"alpha","dist_pct":6.58,"price":761.0,"ma":714.0,"perf_30d":-1.99,"ref_asset":"S&P 500","ma_label":"MA200","updated":"2026-09-15T17:57:57.787056+00:00"};
+window.__MODE_TRADFI_LIVE__={"mode":"alpha","dist_pct":6.58,"price":761.0,"ma":714.0,"perf_30d":-1.99,"ref_asset":"S&P 500","ma_label":"MA200","updated":"2026-09-15T22:29:57.492748+00:00"};
